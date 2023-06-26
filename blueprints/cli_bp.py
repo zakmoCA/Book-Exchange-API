25885cee6460be1946556eaabc830e0496cb6f59
zakmoCA <13905@coderacademy.edu.au>
2023-06-26 23:25:48 +1000

    configuring flask app

--- blueprints/cli_bp.py
+++ blueprints/cli_bp.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from init import db
+from init import db, bcrypt
 from models.user import User
 from models.book import Book
 from datetime import date
@@ -11,3 +11,37 @@ def create_db():
     db.drop_all()
     db.create_all()
     print('Tables created successfully')
+
+@cli_bp.cli.command('seed')
+def seed_db():
+    users = [
+        User(
+        username='TestUser1',
+        email='testuser1@spam.com',
+        password=bcrypt.generate_password_hash('password').decode('utf-8')
+        )
+    ]
+
+    db.session.query(User).delete()
+    db.session.add_all(users)
+    db.session.commit()
+
+    books = [
+        Book(
+        title = 'Harry Potter',
+        author = 'JK Rowling',
+        genre = 'Fantasy',
+        publication_year = 1996
+        )
+    ]
+
+    # Truncate the Book table
+    db.session.query(Book).delete()
+    
+    # Add the book to the session (transaction)
+    db.session.add_all(books)
+    
+    # Commit the transaction to the database
+    db.session.commit()
+
+    print('Models seeded')
