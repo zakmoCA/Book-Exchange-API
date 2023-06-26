108707fb8f3b7ab6af9874ffc9469e6bf9e0646a
zakmoCA <13905@coderacademy.edu.au>
2023-06-26 23:32:18 +1000

    location model

---
+++ models/location.py
@@ -0,0 +1,25 @@
+from init import db, ma
+from marshmallow import fields
+from marshmallow.validate import Length
+
+VALID_STATUSES = ['Requested', 'Accepted', 'Declined', 'Completed']
+
+class Location(db.Model):
+    __tablename__ = 'locations'
+
+    id = db.Column(db.Integer, primary_key=True)
+    city = db.Column(db.String(100))
+    state = db.Column(db.String(100))
+    country = db.Column(db.String(100))
+
+    users = db.relationship('User', backref='location', lazy='dynamic')
+
+class LocationSchema(ma.Schema):
+    city = fields.String(required=True, validate=Length(min=1, max=100))
+    state = fields.String(required=True, validate=Length(min=1, max=100))
+    country = fields.String(required=True, validate=Length(min=1, max=100))
+ 
+    class Meta:
+        fields = ('id', 'city', 'state', 'country')
+        ordered = True
+
