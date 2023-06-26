37d6228caf5e06248bfc487fb8398a25ed2fdf7b
zakmoCA <13905@coderacademy.edu.au>
2023-06-26 20:36:47 +1000

    book model

---
+++ models/book.py
@@ -0,0 +1,23 @@
+from init import db, ma
+from marshmallow import fields
+from marshmallow.validate import Length
+
+class Book(db.Model):
+    __tablename__ = 'books'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200))
+    author = db.Column(db.String(100))
+    genre = db.Column(db.String(100), nullable=True)
+    publication_year = db.Column(db.Integer, nullable=True)
+
+
+class BookSchema(ma.Schema):
+    title = fields.String(required=True, validate=Length(min=1, max=200, error='Title must be between 1 and 200 characters long'))
+    author = fields.String(required=True, validate=Length(min=1, max=100, error='Author name must be between 1 and 100 characters long'))
+    genre = fields.String(validate=Length(max=100, error='Genre must be up to 100 characters long'))
+    publication_year = fields.Integer()
+
+    class Meta:
+        fields = ('id', 'title', 'author', 'genre', 'publication_year', 'owner_id')
+        ordered = True
