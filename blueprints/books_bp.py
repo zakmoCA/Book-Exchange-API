bfd3723561baf42ed2165af3962f537beef6ce38
zakmoCA <13905@coderacademy.edu.au>
2023-06-28 04:12:37 +1000

    search book via title or author

--- blueprints/books_bp.py
+++ blueprints/books_bp.py
@@ -2,6 +2,7 @@ from flask import Blueprint, request
 from models.book import Book, BookSchema
 from models.user import User, UserSchema
 from init import db
+from sqlalchemy import or_
 
 books_bp = Blueprint('books', __name__, url_prefix='/books') # prefix means we don't need to include 'books' in the routes
 
@@ -31,6 +32,10 @@ def search_books():
     search_query = request.args.get('query')
   
     # using SQLAlchemy's ilike function for case insensitive search
-    stmt = db.select(Book).filter(Book.title.ilike(f"%{search_query}%")) # THINK ABOUT SETTING LOWER BOUNDS FOR LENGTH OF SEARCH QUERY
+    stmt = db.select(Book).filter(
+        or_(# or_ is used to create a SQL query that will match either title or author
+            Book.title.ilike(f"%{search_query}%"), # THINK ABOUT SETTING LOWER BOUNDS FOR LENGTH OF SEARCH QUERY
+            Book.author.ilike(f"%{search_query}")
+        ))
     books = db.session.scalars(stmt).all()
     return BookSchema(many=True).dump(books), 200
