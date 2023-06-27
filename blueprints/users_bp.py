d6620ccf620369599d2068479a19a0f9587b818e
zakmoCA <13905@coderacademy.edu.au>
2023-06-28 05:57:27 +1000

    get specific user

--- blueprints/users_bp.py
+++ blueprints/users_bp.py
@@ -14,3 +14,14 @@ def all_users():
     stmt = db.select(User).order_by(User.id)
     users = db.session.scalars(stmt).all()
     return UserSchema(many=True).dump(users), 200
+
+# GET specific user
+@users_bp.route('/<int:user_id>')
+def one_book(user_id):
+    
+    # select * from books;
+    stmt = db.select(User).filter_by(id=user_id)
+    user = db.session.scalar(stmt)
+    if user is None:
+        return {"error": "User not found"}, 404
+    return UserSchema().dump(user), 200
