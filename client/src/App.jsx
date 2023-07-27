c3468f9d10dda8cb7a5c91d5f91dd3b276058f09
zakmoCA <13905@coderacademy.edu.au>
2023-07-27 15:59:34 +1000

    frontend registeration configured

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,15 @@
-import { useState } from 'react';
+import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [loginEmail, setLoginEmail] = useState('')
+  const [loginPassword, setLoginPassword] = useState('')
+
+  const [registerEmail, setRegisterEmail] = useState('')
+  const [registerPassword, setRegisterPassword] = useState('')
+
+  const [username, setUsername] = useState('')
+  const [location, setLocation] = useState('')
 
   async function handleLogin() {
     const response = await fetch('http://127.0.0.1:5000/auth/login', {
@@ -12,19 +18,44 @@ function App() {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        email: email,
-        password: password
+        email: loginEmail,
+        password: loginPassword
       })
-    });
+    })
 
-    const data = await response.json();
+    const data = await response.json()
 
     if (response.ok) {
       // Login was successful
-      console.log('Logged in:', data);
+      console.log('Logged in:', data)
     } else {
       // Something went wrong
-      console.log('Error:', data);
+      console.log('Error:', data)
+    }
+  }
+
+  async function handleRegister() {
+    const response = await fetch('http://127.0.0.1:5000/auth/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email: registerEmail,
+        password: registerPassword,
+        username: username,
+        location_id: location
+      })
+    })
+
+    const data = await response.json()
+
+    if (response.ok) {
+      // Login was successful
+      console.log('Logged in:', data)
+    } else {
+      // Something went wrong
+      console.log('Error:', data)
     }
   }
   
@@ -33,15 +64,29 @@ function App() {
       <div className="container">
         <div className="login-fields">
           <label htmlFor="" className="login-email">Email: </label>
-          <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+          <input type="text" value={loginEmail} onChange={(e) => setLoginEmail(e.target.value)} />
           <br></br>
           <label htmlFor="" className="password-login">Password: </label>
-          <input type="text" value={password} onChange={(e) => setPassword(e.target.value)} />
+          <input type="text" value={loginPassword} onChange={(e) => setLoginPassword(e.target.value)} />
           <button onClick={handleLogin}>Login</button>
+        </div>
+        <div className="register-fields">
+          <label htmlFor="" className="register-email">Email: </label>
+          <input type="text" value={registerEmail} onChange={(e) => setRegisterEmail(e.target.value)} />
+          <br />
+          <label htmlFor="" className="register-username">Username: </label>
+          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+          <br></br>
+          <label htmlFor="" className="register-location">Location: </label>
+          <input type="text" value={location} onChange={(e) => setLocation(e.target.value)} />
+          <br></br>
+          <label htmlFor="" className="register-password">Password: </label>
+          <input type="text" value={registerPassword} onChange={(e) => setRegisterPassword(e.target.value)} />
+          <button onClick={handleRegister}>Register</button>
         </div>
       </div>
     </>
   )
 }
 
-export default App;
+export default App
